475b917f8771128ad712196eeb9dc7c1bf9b0363
tangqingbo <tangqingbo@163.com>
2021-08-27 23:04:32 +0800

    first commit

---
+++ blog-api/src/main/java/com/zhuang/blog/BlogApplication.java
@@ -0,0 +1,18 @@
+package com.zhuang.blog;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author tangqingbo
+ * @Date 2021/8/25 22:11
+ * @Version 1.0
+ */
+@SpringBootApplication
+@MapperScan(basePackages = "com.zhuang.blog.dao")
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
---
+++ blog-api/src/main/java/com/zhuang/blog/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package com.zhuang.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Author tangqingbo
+ * @Date 2021/8/25 22:25
+ * @Version 1.0
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 跨域解决
+     */
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("access-control-allow-headers",
+                        "access-control-allow-methods",
+                        "access-control-allow-origin",
+                        "access-control-max-age",
+                        "X-Frame-Options")
+                .allowCredentials(false).maxAge(3600);
+        super.addCorsMappings(registry);
+    }
+}
